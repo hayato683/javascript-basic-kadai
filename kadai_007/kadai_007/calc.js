60956468357d5d40579bb5ad52c31b060cdb8c7a
hayato683 <148669953+hayato683@users.noreply.github.com>
2024-12-04 15:13:52 +0900

    Add files via upload

---
+++ kadai_007/kadai_007/calc.js
@@ -0,0 +1,11 @@
+for(let number = 1; number<=15; number++){
+if(number % 3 === 0 && number % 5 ===0){
+    console.log('3と5の倍数です');
+}
+else if (number % 5 === 0){
+    console.log('5の倍数です');
+}
+else if(number % 3 === 0){
+    console.log('3の倍数です');
+}
+}
